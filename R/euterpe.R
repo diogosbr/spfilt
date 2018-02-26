71cdbd7ec2acd99b25054e4e7cf8ab20b0b5bfb5
Diogo <diogosbr@gmail.com>
2018-02-26 09:29:42 -0300

    add a 'euterpe' object

---
+++ R/euterpe.R
@@ -0,0 +1,7 @@
+#' Euterpe edulis data.
+#'
+#' 
+#'
+#' @format Distribution data for Euterpe edulis (a widely distributed palm, occurring continuously throughout the Atlantic Forest). Downloaded from GBIF with the gbif function from dismo package.
+#' @references \url{http://www.gbif.org}
+"euterpe"
