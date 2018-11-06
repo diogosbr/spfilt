43009d3bbd44ecff1024b93699bc2f4845f997a3
Diogo <diogosbr@gmail.com>
2018-11-06 15:19:58 -0200

    fix a big bug

--- R/remove.dups.R
+++ R/remove.dups.R
@@ -8,7 +8,7 @@
 #' @param mask String.Name of shape column containing the standard to be searched for.
 #'
 #' @details The
-#' 
+#'
 #' @return a data frame
 #'
 #' @author Diogo S. B. Rocha
@@ -22,40 +22,70 @@
 #'
 #' @export
 
-remove.dups  <-  function(pts, 
-                          mask = example, 
-                          na.rm = FALSE) {
-  
-  if (names(pts)[1]== "sp"  | names(pts)[1]== "lon" | names(pts)[1]== "lat") {
+remove.dups  <-  function(pts,
+                          mask = example,
+                          na.rm = FALSE,
+                          dup = T) {
+  if (names(pts)[1] == "sp"  |
+      names(pts)[1] == "lon" | names(pts)[1] == "lat") {
     species <- unique(pts$sp)
     
+    if (dup == T) {
+      message("cleaning duplicates")
+      dupls <- !duplicated(pts[, c('sp', 'lon', 'lat')])
+      registros <- registros[dupls,]
+    }
+    
     if (exists("mask")) {
-      if(length(species>1)){
-        for(specie in species){
+      if (length(species > 1)) {
+        pb <- txtProgressBar(min = 1,
+                             max = length(species),
+                             style = 3)
+        registros.unique = c()
+        for (i in 1:length(species)) {
+          setTxtProgressBar(pb, i)
           # Selecionar pontos espacialmente únicos #
-          cell <- cellFromXY(mask, pts[pts$sp == specie,c("lon","lat")])  # get the cell number for each point
+          cell <-
+            cellFromXY(mask, pts[pts$sp == species[i], c("lon", "lat")])  # get the cell number for each point
           dup <- duplicated(cell)
-          pts1 <- pts[pts$sp == specie | !dup, ]  # select the records that are not duplicated
+          pts2 <-
+            pts[pts$sp == species[i] |
+                  !dup,]  # select the records that are not duplicated
+          registros.unique = rbind(registros.unique, pts2)
         }
-		if(na.rm == TRUE){
-            pts1 <- pts1[!is.na(extract(mask, pts1[,c('lon','lat')])), ]  #selecionando apenas pontos que tem valor de raster
+        if (na.rm == TRUE) {
+          message("\n Removing NA \n")
+          pts1 <-
+            pts1[!is.na(extract(mask, pts1[, c('lon', 'lat')])),]  #selecionando apenas pontos que tem valor de raster
         }
-      }else{
+      } else{
         # Selecionar pontos espacialmente únicos #
-        cell <- cellFromXY(mask, pts[,c("lon","lat")])  # get the cell number for each point
+        cell <-
+          cellFromXY(mask, pts[, c("lon", "lat")])  # get the cell number for each point
         dup <- duplicated(cell)
-        pts1 <- pts[!dup, ]  # select the records that are not duplicated
+        pts1 <-
+          pts[!dup,]  # select the records that are not duplicated
         
-        if(na.rm == TRUE){
-          pts1 <- pts1[!is.na(extract(mask, pts1[,c('lon','lat')])), ]  #selecionando apenas pontos que tem valor de raster
+        if (na.rm == TRUE) {
+          pts1 <-
+            pts1[!is.na(extract(mask, pts1[, c('lon', 'lat')])),]  #selecionando apenas pontos que tem valor de raster
         }
       }
       
       cat(dim(pts)[1] - dim(pts1)[1], "points removed\n")
       cat(dim(pts1)[1], "spatially unique points\n")
-    } else (cat("Indicate the object with the predictive variables"))
-  }else{stop("The first three columns should be 'sp', 'lon' and 'lat' necessarily in this order")}
-
+    } else
+      (cat("Indicate the object with the predictive variables"))
+  } else{
+    stop("The first three columns should be 'sp', 'lon' and 'lat' necessarily in this order")
+  }
+  
   message("Finished ...\n")
-  invisible(pts1)
+  if (exists(pts1)) {
+    invisible(pts1)
+  }
+  if (!is.null(registros.unique)) {
+    invisible(registros.unique)
+  }
+  
 }
